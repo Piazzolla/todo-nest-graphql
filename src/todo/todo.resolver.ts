83250a059f1bc37fe78b8d8aed09067ffe86388e
Mariano <macacozzz@hotmail.com>
2024-11-05 15:36:53 -0300

    get todo by id

--- src/todo/todo.resolver.ts
+++ src/todo/todo.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver, Int } from '@nestjs/graphql';
 import { Todo } from './entity/todo.entity';
 import { TodoService } from './todo.service';
 
@@ -14,8 +14,9 @@ export class TodoResolver {
         return this.todoService.findAll();
     }
 
-    findOne() {
-        return [];
+    @Query( () => Todo, { name: 'todo'} )
+    findOne( @Args('id', {type: () => Int}) id: number) {
+        return this.todoService.findOne( id );
     }
 
     createTodo() {
